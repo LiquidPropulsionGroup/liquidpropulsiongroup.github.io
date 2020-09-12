c0ccaca5ddee762a6adfcc638d0695dbac7eab8b
ellie <ellieyhc@gmail.com>
2020-09-11 17:48:35 -0700

    404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,17 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Styles from '../styles/404.module.css'
+import PlutoImg from '../images/pluto.svg'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404</h1>
+    <div className={Styles.nonexistent}>
+      <img src={PlutoImg} alt='pluto' />
+      <p>You're trying to visit a page that doesn't exist... big sad :( </p>
+    </div>
   </Layout>
 )
 
