2c1d5464ab2c8ee6b28dc704ab0c8979638ade5b
ellie <ellieyhc@gmail.com>
2020-09-12 12:09:54 -0700

    Clean up

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,22 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-
-// You can delete this file if you're not using it
-// const fs = require("fs")
-// const yaml = require("js-yaml")
-
-// exports.createPages = ({ actions }) => {
-//   const { createPage } = actions
-//   const ymlDoc = yaml.safeLoad(fs.readFileSync("./content/index.yaml", "utf-8"))
-//   ymlDoc.forEach(element => {
-//     createPage({
-//       path: element.path,
-//       component: require.resolve("./src/templates/basicTemplate.js"),
-//       context: {
-//         pageContent: element.content,
-//         links: element.links,
-//       },
-//     })
-//   })
-// }
