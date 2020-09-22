608b06e0fa6e46796350c975d7beba7a2a45c4a6
ellie <ellieyhc@gmail.com>
2020-09-22 13:52:44 -0700

    Section styling fix & use dimensions instead of orientation

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,6 @@ import FlexLayout from '../components/flexlayout'
 import Logo from '../images/logo.png'
 import RotateImg from '../images/rotate.svg'
 import ContactLinks from '../components/contactLinks'
-import DeviceOrientation, { Orientation } from 'react-screen-orientation'
 
 const ListLink = props => (
   <li>
@@ -15,12 +14,18 @@ const ListLink = props => (
 )
 
 export default function Layout({ children }) {
-  const [width, setWidth] = useState(window.innerWidth)
+  const [dimensions, setDimentions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  })
 
   useLayoutEffect(() => {
     function handleResize() {
-      if (Math.abs(window.innerWidth - width) > 300) {
-        setWidth(window.innerWidth);
+      if (Math.abs(window.innerWidth - dimensions.width) > 300 || Math.abs(window.innerHeight - dimensions.height) > 200) {
+        setDimentions({
+          height: window.innerHeight,
+          width: window.innerWidth,
+        })
       }
     }
 
@@ -29,9 +34,42 @@ export default function Layout({ children }) {
     return () => {
       window.removeEventListener('resize', handleResize)
     }
-  }, [width]);
+  }, [dimensions]);
 
-  if (width >= 1024)
+  if (dimensions.width >= 1024)
+    return (
+      <div className={Styles.layout}>
+        <header className={Styles.header}>
+          <FlexLayout justify='space-between' align='center'>
+            <FlexLayout align='center'>
+              <Link to="/">
+                <img src={Logo} alt='LPG Logo' />
+              </Link>
+              <Link to="/">
+                <h1>Liquid Propulsion Group</h1>
+              </Link>
+            </FlexLayout>
+            <ul>
+              {PageData.map((data, idx) => (
+                <ListLink to={data.path} key={`list_link_${idx}`}>{data.title}</ListLink>
+              ))}
+            </ul>
+          </FlexLayout>
+        </header>
+        {children}
+        <footer className={Styles.footer}>
+          <div className={Styles.footerContent}>
+            <img src={Logo} alt='LPG Logo' />
+              Liquid Propulsion Group
+            </div>
+          <ContactLinks />
+          <div className={Styles.creator}>
+            <p>Designed & Made by <a href={'https://ellieyhcheng.github.io/'}>Ellie Cheng</a></p>
+          </div>
+        </footer>
+      </div>
+    )
+  else if (dimensions.height > dimensions.width)
     return (
       <div className={Styles.layout}>
         <header className={Styles.header}>
@@ -66,76 +104,40 @@ export default function Layout({ children }) {
     )
   else
     return (
-      <DeviceOrientation lockOrientation={'portrait'}>
-        <Orientation orientation='portrait' alwaysRender={false}>
-          <div className={Styles.layout}>
-            <header className={Styles.header}>
-              <FlexLayout justify='space-between' align='center'>
-                <FlexLayout align='center'>
-                  <Link to="/">
-                    <img src={Logo} alt='LPG Logo' />
-                  </Link>
-                  <Link to="/">
-                    <h1>Liquid Propulsion Group</h1>
-                  </Link>
-                </FlexLayout>
-                <ul>
-                  {PageData.map((data, idx) => (
-                    <ListLink to={data.path} key={`list_link_${idx}`}>{data.title}</ListLink>
-                  ))}
-                </ul>
-              </FlexLayout>
-            </header>
-            {children}
-            <footer className={Styles.footer}>
-              <div className={Styles.footerContent}>
+      <div className={Styles.layout}>
+        <header className={Styles.header}>
+          <FlexLayout justify='space-between' align='center'>
+            <FlexLayout align='center'>
+              <Link to="/">
                 <img src={Logo} alt='LPG Logo' />
+              </Link>
+              <Link to="/">
+                <h1>Liquid Propulsion Group</h1>
+              </Link>
+            </FlexLayout>
+            <ul>
+              {PageData.map((data, idx) => (
+                <ListLink to={data.path} key={`list_link_${idx}`}>{data.title}</ListLink>
+              ))}
+            </ul>
+          </FlexLayout>
+        </header>
+        <div className={Styles.rotate}>
+          <img src={RotateImg} alt='' />
+          <p>Please rotate your device.</p>
+          <p>This page is best viewed in portrait orientation.</p>
+          <p>If this message doesn't disappear after you've rotated to portrait orientation, try refreshing the page.</p>
+        </div>
+        <footer className={Styles.footer}>
+          <div className={Styles.footerContent}>
+            <img src={Logo} alt='LPG Logo' />
               Liquid Propulsion Group
             </div>
-              <ContactLinks />
-              <div className={Styles.creator}>
-                <p>Designed & Made by <a href={'https://ellieyhcheng.github.io/'}>Ellie Cheng</a></p>
-              </div>
-            </footer>
+          <ContactLinks />
+          <div className={Styles.creator}>
+            <p>Designed & Made by <a href={'https://ellieyhcheng.github.io/'}>Ellie Cheng</a></p>
           </div>
-        </Orientation>
-        <Orientation orientation='landscape' alwaysRender={false}>
-          <div className={Styles.layout}>
-            <header className={Styles.header}>
-              <FlexLayout justify='space-between' align='center'>
-                <FlexLayout align='center'>
-                  <Link to="/">
-                    <img src={Logo} alt='LPG Logo' />
-                  </Link>
-                  <Link to="/">
-                    <h1>Liquid Propulsion Group</h1>
-                  </Link>
-                </FlexLayout>
-                <ul>
-                  {PageData.map((data, idx) => (
-                    <ListLink to={data.path} key={`list_link_${idx}`}>{data.title}</ListLink>
-                  ))}
-                </ul>
-              </FlexLayout>
-            </header>
-            <div className={Styles.rotate}>
-              <img src={RotateImg} alt='' />
-              <p>Please rotate your device.</p>
-              <p>This page is best viewed in portrait orientation.</p>
-              <p>If this message doesn't disappear after you've rotated to portrait orientation, try refreshing the page.</p>
-            </div>
-            <footer className={Styles.footer}>
-              <div className={Styles.footerContent}>
-                <img src={Logo} alt='LPG Logo' />
-              Liquid Propulsion Group
-            </div>
-              <ContactLinks />
-              <div className={Styles.creator}>
-                <p>Designed & Made by <a href={'https://ellieyhcheng.github.io/'}>Ellie Cheng</a></p>
-              </div>
-            </footer>
-          </div>
-        </Orientation>
-      </DeviceOrientation>
+        </footer>
+      </div>
     )
 }
