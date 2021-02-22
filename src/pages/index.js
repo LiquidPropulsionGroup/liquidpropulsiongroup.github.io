9a2a14c3b4e8276a21b4e968cfe84bba9ce764e0
ellie <ellieyhc@gmail.com>
2021-02-21 17:16:58 -0800

    Add slideshow code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,23 @@
 import IndexData from '../content/index.yaml'
+import SlideshowData from '../content/slideshow.yaml'
 import Layout from "../components/layout"
 import React, { useLayoutEffect, useState } from "react"
-import RocketImg from '../images/rocket.svg'
-import RocketSmallImg from '../images/rocket-small.svg'
 import SEO from '../components/seo'
 import Styles from '../styles/index.module.css'
 import ReactMarkdown from "react-markdown"
+import RightArrow from '../images/right-arrow.svg'
+import LeftArrow from '../images/left-arrow.svg'
 
 export default function Home() {
   const [width, setWidth] = useState(2060)
+  const [slideIdx, setSlideIdx] = useState(0)
 
-  useLayoutEffect(() => (
+  const [numSlides, setNumSlides] = useState(100);
+
+  useLayoutEffect(() => {
     setWidth(window.innerWidth)
-  ), [])
+    setNumSlides(SlideshowData.length)
+  }, [])
 
   useLayoutEffect(() => {
     function handleResize() {
@@ -38,15 +43,26 @@ export default function Home() {
     <Layout>
       <SEO />
       <div className={Styles.landing}>
-        {width < 768 ? (
-          <>
-            <h1>Liquid Propulsion Group</h1>
-            <h2>Making Rocket Engines</h2>
-          </>
-        ) : (
-            <h1>Making Rocket Engines</h1>
-          )}
-        <img src={width < 1024 ? RocketSmallImg : RocketImg} alt='' />
+        <div className={Styles.slideshow}>
+          <div className={Styles.arrowWrapper}>
+            <div className={Styles.arrow} onClick={() => setSlideIdx((slideIdx) => slideIdx - 1 < 0 ? slideIdx - 1 + numSlides : slideIdx - 1 )}>
+              <img src={LeftArrow} alt='' />
+            </div>
+            <div className={Styles.arrow}>
+              <img src={RightArrow} alt='' onClick={() => setSlideIdx((slideIdx) => (slideIdx + 1) % numSlides)}/>
+            </div>
+          </div>
+          {SlideshowData.map((data, idx) => ( 
+            <div key={`slide_${idx}`} className={Styles.slide} style={
+              { 
+                opacity: idx === slideIdx ? 1 : 0, 
+                transform: (idx === (slideIdx - 1 < 0 ? slideIdx - 1 + numSlides : slideIdx - 1)) ? `translate(-100%, 0)` : (idx === (slideIdx + 1) % numSlides ? `translate(100%, 0)` : `translate(${100 * (idx - slideIdx)}%, 0px)`)}
+              }>
+              <img src={require(`../images/${data.image}`)} alt={`${data.caption}`} />
+              <p>{data.caption}</p>
+            </div>
+          ))}
+        </div>
       </div>
       <div>
         {IndexData.map((data, idx) => (
